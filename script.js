c60dd676c2d9dbe0d969f322fc2b61e4796102b2
Ajithesh9 <105075180+Ajithesh9@users.noreply.github.com>
2025-01-23 16:03:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function getWeather() {
+  const apiKey = "75c72380d497bb63c907c1c44480594f";
+  const city = document.getElementById("city").value;
+}
+if (!city) {
+  alert("Please enter a city");
+  return;
+}
+
+}
+
